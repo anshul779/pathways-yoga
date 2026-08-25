c962c6b4416359cbb70c94b54c487767bc21c30b
anshul779 <anshul@mantra.care>
2026-08-25 12:37:43 +0530

    Remove pathway header bar

--- app/pathway-10/layout.tsx
+++ app/pathway-10/layout.tsx
@@ -1,5 +1,4 @@
 ﻿import "./globals.css";
-import PathwayHeader from "@/app/components/PathwayHeader";
 
 export default function PathwayLayout({
   children,
@@ -8,7 +7,6 @@ export default function PathwayLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col pathway-10-container">
-      <PathwayHeader currentId={10} />
       <div className="flex-1">
         {children}
       </div>
